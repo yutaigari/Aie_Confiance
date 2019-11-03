360a914d1a8f0cd96c0d35bc6619f656d43baeae
yutaigari <soccer1604422@icloud.com>
2019-11-03 06:56:03 +0000

    finish signup login

---
+++ db/migrate/20191030092821_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :self_introduction, :string, limit: 500
+    add_column :users, :sex, :integer, null: false, default: 0
+    add_column :users, :img_name, :string
+  end
+end
