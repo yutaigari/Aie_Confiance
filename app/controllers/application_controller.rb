81feb97b6226924123f4cddb6811696ac37c1b42
yutaigari <soccer1604422@icloud.com>
2019-11-03 15:00:06 +0000

    finish user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction sex img_name))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction sex img_name))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'users/registrations'} 
+  #user編集後にusers/showに飛ばすためにusers/registrationsでafter_update_path_for(resource)を定義したので設定を変更する必要がある
+  
   root "top#index"
-
+  
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
