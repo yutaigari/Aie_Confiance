d4c42011a2d84c18d6487c975b000c370dba4aa5
yutaigari <soccer1604422@icloud.com>
2019-12-10 16:02:01 +0000

    finish ajax_first

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,20 @@ class FavoritesController < ApplicationController
     @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: @post_image.id)
     favorite.save
-    redirect_to post_image_url(@post_image)
+    respond_to do |format|
+      format.html {redirect_to post_image_url(@post_image)}
+      format.js
+    end
   end
   
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: @post_image.id)
     favorite.destroy
-    redirect_to post_image_url(@post_image)
+    respond_to do |format|
+      format.html {redirect_to post_image_url(@post_image)}
+      format.js
+    end
   end
   
 end
